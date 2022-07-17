491d239f7536572d7056724d2aa2ab12d0a851bd
Maryam <maryam.meghdadi@stud.uni-saarland.de>
2022-07-17 16:55:18 +0200

    modify attention_unet (smaller)

--- models/Attention_UNet.py
+++ models/Attention_UNet.py
@@ -4,61 +4,36 @@ import torch.nn.functional as F
 from torch.nn import init
 
 
-def init_weights(net, init_type='normal', gain=0.02):
-    def init_func(m):
-        classname = m.__class__.__name__
-        if hasattr(m, 'weight') and (classname.find('Conv') != -1 or classname.find('Linear') != -1):
-            if init_type == 'normal':
-                init.normal_(m.weight.data, 0.0, gain)
-            elif init_type == 'xavier':
-                init.xavier_normal_(m.weight.data, gain=gain)
-            elif init_type == 'kaiming':
-                init.kaiming_normal_(m.weight.data, a=0, mode='fan_in')
-            elif init_type == 'orthogonal':
-                init.orthogonal_(m.weight.data, gain=gain)
-            else:
-                raise NotImplementedError('initialization method [%s] is not implemented' % init_type)
-            if hasattr(m, 'bias') and m.bias is not None:
-                init.constant_(m.bias.data, 0.0)
-        elif classname.find('BatchNorm2d') != -1:
-            init.normal_(m.weight.data, 1.0, gain)
-            init.constant_(m.bias.data, 0.0)
-
-    print('initialize network with %s' % init_type)
-    net.apply(init_func)
-
-
 class conv_block(nn.Module):
-    def __init__(self, ch_in, ch_out):
-        super(conv_block, self).__init__()
+    def __init__(self,ch_in,ch_out):
+        super(conv_block,self).__init__()
         self.conv = nn.Sequential(
-            nn.Conv2d(ch_in, ch_out, kernel_size=3, stride=1, padding=1, bias=True),
-            nn.BatchNorm2d(ch_out),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(ch_out, ch_out, kernel_size=3, stride=1, padding=1, bias=True),
+            nn.Conv2d(ch_in, ch_out, kernel_size=3,stride=1,padding=1,bias=True),
             nn.BatchNorm2d(ch_out),
             nn.ReLU(inplace=True)
+            # nn.Conv2d(ch_out, ch_out, kernel_size=3,stride=1,padding=1,bias=True),
+            # nn.BatchNorm2d(ch_out),
+            # nn.ReLU(inplace=True)
         )
 
-    def forward(self, x):
+    def forward(self,x):
         x = self.conv(x)
         return x
 
 
 class up_conv(nn.Module):
-    def __init__(self, ch_in, ch_out):
-        super(up_conv, self).__init__()
+    def __init__(self,ch_in,ch_out):
+        super(up_conv,self).__init__()
         self.up = nn.Sequential(
             nn.Upsample(scale_factor=2),
-            nn.Conv2d(ch_in, ch_out, kernel_size=3, stride=1, padding=1, bias=True),
-            nn.BatchNorm2d(ch_out),
-            nn.ReLU(inplace=True)
+            nn.Conv2d(ch_in,ch_out,kernel_size=3,stride=1,padding=1,bias=True),
+		    nn.BatchNorm2d(ch_out),
+			nn.ReLU(inplace=True)
         )
 
-    def forward(self, x):
+    def forward(self,x):
         x = self.up(x)
         return x
-
 
 class Attention_block(nn.Module):
     def __init__(self, F_g, F_l, F_int):
@@ -100,11 +75,11 @@ class AttU_Net(nn.Module):
         self.Conv2 = conv_block(ch_in=64, ch_out=128)
         self.Conv3 = conv_block(ch_in=128, ch_out=256)
         self.Conv4 = conv_block(ch_in=256, ch_out=512)
-        self.Conv5 = conv_block(ch_in=512, ch_out=1024)
+        # self.Conv5 = conv_block(ch_in=512, ch_out=1024)
 
-        self.Up5 = up_conv(ch_in=1024, ch_out=512)
-        self.Att5 = Attention_block(F_g=512, F_l=512, F_int=256)
-        self.Up_conv5 = conv_block(ch_in=1024, ch_out=512)
+        # self.Up5 = up_conv(ch_in=1024, ch_out=512)
+        # self.Att5 = Attention_block(F_g=512, F_l=512, F_int=256)
+        # self.Up_conv5 = conv_block(ch_in=1024, ch_out=512)
 
         self.Up4 = up_conv(ch_in=512, ch_out=256)
         self.Att4 = Attention_block(F_g=256, F_l=256, F_int=128)
@@ -133,16 +108,16 @@ class AttU_Net(nn.Module):
         x4 = self.Maxpool(x3)
         x4 = self.Conv4(x4)
 
-        x5 = self.Maxpool(x4)
-        x5 = self.Conv5(x5)
+        # x5 = self.Maxpool(x4)
+        # x5 = self.Conv5(x5)
 
         # decoding + concat path
-        d5 = self.Up5(x5)
-        x4 = self.Att5(g=d5, x=x4)
-        d5 = torch.cat((x4, d5), dim=1)
-        d5 = self.Up_conv5(d5)
+        # d5 = self.Up5(x5)
+        # x4 = self.Att5(g=d5, x=x4)
+        # d5 = torch.cat((x4, d5), dim=1)
+        # d5 = self.Up_conv5(d5)
 
-        d4 = self.Up4(d5)
+        d4 = self.Up4(x4)
         x3 = self.Att4(g=d4, x=x3)
         d4 = torch.cat((x3, d4), dim=1)
         d4 = self.Up_conv4(d4)
